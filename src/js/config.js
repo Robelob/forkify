c165dd8cd15dafd2a56ee343cb82d189fa143a22
robelob <robel.mukebede@gmail.com>
2021-04-09 15:44:57 +0200

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '65a7bf3f-6c24-4044-9b64-6c383c083328';
+export const MODAL_CLOSE_SEC = 2.5;
